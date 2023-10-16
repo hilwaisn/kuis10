01ba7dda69c43d5cafafaf20e60a1839c029deba
Hilwa Isnaini Marfua <Hilwa Isnaini Marfua@HILWAISN19>
2023-10-16 21:07:23 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace kuis10
+{
+    partial class KuisPertemuan10
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlBiru = new System.Windows.Forms.Panel();
+            this.btnKali = new System.Windows.Forms.Button();
+            this.btnKurang = new System.Windows.Forms.Button();
+            this.btnBagi = new System.Windows.Forms.Button();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.txtHasil = new System.Windows.Forms.TextBox();
+            this.txtAngka2 = new System.Windows.Forms.TextBox();
+            this.lblHasil = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lblAngka1 = new System.Windows.Forms.Label();
+            this.txtAngka1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlBiru.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlBiru
+            // 
+            this.pnlBiru.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pnlBiru.Controls.Add(this.btnKali);
+            this.pnlBiru.Controls.Add(this.btnKurang);
+            this.pnlBiru.Controls.Add(this.btnBagi);
+            this.pnlBiru.Controls.Add(this.btnTambah);
+            this.pnlBiru.Controls.Add(this.txtHasil);
+            this.pnlBiru.Controls.Add(this.txtAngka2);
+            this.pnlBiru.Controls.Add(this.lblHasil);
+            this.pnlBiru.Controls.Add(this.lbl2);
+            this.pnlBiru.Controls.Add(this.lblAngka1);
+            this.pnlBiru.Controls.Add(this.txtAngka1);
+            this.pnlBiru.Location = new System.Drawing.Point(-4, 109);
+            this.pnlBiru.Name = "pnlBiru";
+            this.pnlBiru.Size = new System.Drawing.Size(814, 348);
+            this.pnlBiru.TabIndex = 0;
+            // 
+            // btnKali
+            // 
+            this.btnKali.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnKali.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKali.Location = new System.Drawing.Point(417, 229);
+            this.btnKali.Name = "btnKali";
+            this.btnKali.Size = new System.Drawing.Size(55, 44);
+            this.btnKali.TabIndex = 9;
+            this.btnKali.Text = "x";
+            this.btnKali.UseVisualStyleBackColor = true;
+            this.btnKali.Click += new System.EventHandler(this.btnKali_Click);
+            // 
+            // btnKurang
+            // 
+            this.btnKurang.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnKurang.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKurang.Location = new System.Drawing.Point(254, 229);
+            this.btnKurang.Name = "btnKurang";
+            this.btnKurang.Size = new System.Drawing.Size(55, 44);
+            this.btnKurang.TabIndex = 8;
+            this.btnKurang.Text = "-";
+            this.btnKurang.UseVisualStyleBackColor = true;
+            this.btnKurang.Click += new System.EventHandler(this.btnKurang_Click);
+            // 
+            // btnBagi
+            // 
+            this.btnBagi.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnBagi.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBagi.Location = new System.Drawing.Point(332, 229);
+            this.btnBagi.Name = "btnBagi";
+            this.btnBagi.Size = new System.Drawing.Size(55, 44);
+            this.btnBagi.TabIndex = 7;
+            this.btnBagi.Text = ":";
+            this.btnBagi.UseVisualStyleBackColor = true;
+            this.btnBagi.Click += new System.EventHandler(this.btnBagi_Click);
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnTambah.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTambah.Location = new System.Drawing.Point(168, 229);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(55, 44);
+            this.btnTambah.TabIndex = 6;
+            this.btnTambah.Text = "+";
+            this.btnTambah.UseVisualStyleBackColor = true;
+            this.btnTambah.Click += new System.EventHandler(this.btnTambah_Click);
+            // 
+            // txtHasil
+            // 
+            this.txtHasil.Enabled = false;
+            this.txtHasil.Location = new System.Drawing.Point(216, 158);
+            this.txtHasil.Name = "txtHasil";
+            this.txtHasil.Size = new System.Drawing.Size(208, 26);
+            this.txtHasil.TabIndex = 5;
+            // 
+            // txtAngka2
+            // 
+            this.txtAngka2.Location = new System.Drawing.Point(216, 104);
+            this.txtAngka2.Name = "txtAngka2";
+            this.txtAngka2.Size = new System.Drawing.Size(208, 26);
+            this.txtAngka2.TabIndex = 4;
+            // 
+            // lblHasil
+            // 
+            this.lblHasil.AutoSize = true;
+            this.lblHasil.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHasil.Location = new System.Drawing.Point(101, 158);
+            this.lblHasil.Name = "lblHasil";
+            this.lblHasil.Size = new System.Drawing.Size(103, 50);
+            this.lblHasil.TabIndex = 3;
+            this.lblHasil.Text = "Hasil      :\r\n\r\n";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(101, 105);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(105, 25);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "Angka 2 :";
+            // 
+            // lblAngka1
+            // 
+            this.lblAngka1.AutoSize = true;
+            this.lblAngka1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAngka1.Location = new System.Drawing.Point(101, 48);
+            this.lblAngka1.Name = "lblAngka1";
+            this.lblAngka1.Size = new System.Drawing.Size(105, 25);
+            this.lblAngka1.TabIndex = 1;
+            this.lblAngka1.Text = "Angka 1 :";
+            // 
+            // txtAngka1
+            // 
+            this.txtAngka1.Location = new System.Drawing.Point(216, 49);
+            this.txtAngka1.Name = "txtAngka1";
+            this.txtAngka1.Size = new System.Drawing.Size(208, 26);
+            this.txtAngka1.TabIndex = 0;
+            this.txtAngka1.TextChanged += new System.EventHandler(this.txtAngka1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(206, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kalukatore";
+            // 
+            // KuisPertemuan10
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(591, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pnlBiru);
+            this.Name = "KuisPertemuan10";
+            this.Text = "Kuis Pertemuan 10";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlBiru.ResumeLayout(false);
+            this.pnlBiru.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlBiru;
+        private System.Windows.Forms.TextBox txtAngka1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnTambah;
+        private System.Windows.Forms.TextBox txtHasil;
+        private System.Windows.Forms.TextBox txtAngka2;
+        private System.Windows.Forms.Label lblHasil;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lblAngka1;
+        private System.Windows.Forms.Button btnKali;
+        private System.Windows.Forms.Button btnKurang;
+        private System.Windows.Forms.Button btnBagi;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kuis10
+{
+    public partial class KuisPertemuan10 : Form
+    {
+        public KuisPertemuan10()
+        {
+            InitializeComponent();
+        }
+        double temp1;
+        double temp2;
+        char oper;
+        bool oper1;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fokus();
+        }
+        private void fokus()
+        {
+            txtHasil.Focus();
+            txtHasil.Select(txtHasil.Text.Length, 1);
+        }
+
+        private void txtAngka1_TextChanged(object sender, EventArgs e)
+        {
+            Button btnAng = (Button)sender;
+            if (txtHasil.Text == "0")
+            {
+                txtHasil.Clear();
+            }
+            if (oper == '=')
+            {
+                oper = ' ';
+                oper1 = false;
+                temp1 = 0;
+            }
+            txtHasil.Text = txtHasil.Text + btnAng.Text;
+            fokus();
+        }
+
+        private void btnKurang_Click(object sender, EventArgs e)
+        {
+            temp1 = temp1 - Convert.ToDouble(temp2);
+        }
+
+        private void btnTambah_Click(object sender, EventArgs e)
+        {
+            temp1 = temp1 + Convert.ToDouble(temp2);
+        }
+
+        private void btnBagi_Click(object sender, EventArgs e)
+        {
+            temp1 = temp1 / Convert.ToDouble(temp2);
+        }
+
+        private void btnKali_Click(object sender, EventArgs e)
+        {
+            temp1 = temp1 * Convert.ToDouble(temp2);
+        }
+    }
+}
